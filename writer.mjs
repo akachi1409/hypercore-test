c593bf5dbdafa29c03498f69c637e05c7fc50ca6
Akachi <86838625+akachi1409@users.noreply.github.com>
2023-02-02 13:44:01 -0500

    try the sample from git repo

--- writer.mjs
+++ writer.mjs
@@ -1,23 +1,27 @@
-import Hyperswarm from "hyperswarm";
-import Hypercore from "hypercore";
-import goodbye from "graceful-goodbye";
-import b4a from "b4a";
+import Hyperswarm from 'hyperswarm'
+import Hypercore from 'hypercore'
+import goodbye from 'graceful-goodbye'
 
-const swarm = new Hyperswarm();
-goodbye(() => swarm.destroy());
+const swarm = new Hyperswarm()
+goodbye(() => swarm.destroy())
 
-const core = new Hypercore("./writer-storage", { valueEncoding: "json" });
+const core = new Hypercore('./reader-storage', process.argv[2])
+await core.ready()
 
-await core.ready();
-console.log("hypercore key:", b4a.toString(core.key, "hex"));
-console.log("core length:", core.length);
-process.stdin.on('data', data=> core.append(data));
-// const firstBlock = await core.get(0, { valueEncoding: "json" });
-// console.log("first block:", firstBlock);
+const foundPeers = core.findingPeers()
+swarm.join(core.discoveryKey)
+swarm.on('connection', conn => core.replicate(conn))
 
-const tempKey = b4a.alloc(32).fill("game-room-002")
-swarm.join(tempKey);
-swarm.on("connection", (conn) => {
-  console.log("connected in mjs");
-  core.replicate(conn);
-});
+// swarm.flush() will wait until *all* discoverable peers have been connected to
+// It might take a while, so don't await it
+// Instead, use core.findingPeers() to mark when the discovery process is completed
+swarm.flush().then(() => foundPeers())
+
+// This won't resolve until either a) the first peer is found, or b) no peers could be found
+await core.update()
+
+let position = core.length
+console.log(`Skipping ${core.length} earlier blocks...`)
+for await (const block of core.createReadStream({ start: core.length, live: true })) {
+  console.log(`Block ${position++}: ${block}`)
+}
