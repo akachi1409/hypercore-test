930e000264c0e096a6eb3f840626ae154262e44c
Akachi <86838625+akachi1409@users.noreply.github.com>
2023-02-02 13:45:24 -0500

    fix

--- writer.mjs
+++ writer.mjs
@@ -1,27 +1,22 @@
+
 import Hyperswarm from 'hyperswarm'
 import Hypercore from 'hypercore'
 import goodbye from 'graceful-goodbye'
+import b4a from 'b4a'
 
 const swarm = new Hyperswarm()
 goodbye(() => swarm.destroy())
 
-const core = new Hypercore('./reader-storage', process.argv[2])
-await core.ready()
+const core = new Hypercore('./writer-storage')
 
-const foundPeers = core.findingPeers()
+// core.key and core.discoveryKey will only be set after core.ready resolves
+await core.ready()
+console.log('hypercore key:', b4a.toString(core.key, 'hex'))
+
+// Append all stdin data as separate blocks to the core
+process.stdin.on('data', data => core.append(data))
+
+// core.discoveryKey is *not* a read capability for the core
+// It's only used to discover other peers who *might* have the core
 swarm.join(core.discoveryKey)
 swarm.on('connection', conn => core.replicate(conn))
-
-// swarm.flush() will wait until *all* discoverable peers have been connected to
-// It might take a while, so don't await it
-// Instead, use core.findingPeers() to mark when the discovery process is completed
-swarm.flush().then(() => foundPeers())
-
-// This won't resolve until either a) the first peer is found, or b) no peers could be found
-await core.update()
-
-let position = core.length
-console.log(`Skipping ${core.length} earlier blocks...`)
-for await (const block of core.createReadStream({ start: core.length, live: true })) {
-  console.log(`Block ${position++}: ${block}`)
-}
