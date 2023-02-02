4dfd7625a7e1628c445bd58fef1619193721ae6c
Akachi <86838625+akachi1409@users.noreply.github.com>
2023-02-02 06:52:54 -0500

    update reader.mjs

--- reader.mjs
+++ reader.mjs
@@ -1,23 +1,30 @@
-import Hyperswarm from 'hyperswarm'
-import Hypercore from 'hypercore'
-import goodbye from 'graceful-goodbye'
+import Hyperswarm from "hyperswarm";
+import Hypercore from "hypercore";
+import goodbye from "graceful-goodbye";
 
-const swarm = new Hyperswarm()
-goodbye(()=> swarm.destroy())
-
-const core = new Hypercore('./reader-storage', process.argv[2])
+const swarm = new Hyperswarm();
+goodbye(() => swarm.destroy());
+// , process.argv[2]
+const core = new Hypercore("/Hyperbee-books");
 await core.ready();
+console.log("block length:", core.length);
 
+console.log("discory key:", process.argv[2])
 const foundPeers = core.findingPeers();
-swarm.join(core.discoveryKey);
-swarm.on('connection', conn => core.replicate(conn));
+swarm.join(process.argv[2]);
+swarm.on("connection", (conn) => {
+    console.log("swarm connected")
+  core.replicate(conn);
+});
 
-swarm.flush().then(()=> foundPeers());
-
+swarm.flush().then(() => foundPeers());
 await core.update();
 
 let position = core.length;
 console.log(`Skipping ${core.length} earlier blocks...`);
-for await (const block of core.createReadStream({start: core.length, live:true})){
-    console.log(`Block ${position++}: ${block}`)
+for await (const block of core.createReadStream({
+  start: core.length,
+  live: true,
+})) {
+  console.log(`Block ${position++}: ${block}`);
 }
--- writer.mjs
+++ writer.mjs
@@ -6,12 +6,15 @@ import b4a from 'b4a';
 const swarm = new Hyperswarm();
 goodbye(()=> swarm.destroy());
 
-const core = new Hypercore('./writer-storage');
+const core = new Hypercore('./writer-storage', { valueEncoding: 'json' });
 
 await core.ready();
 console.log("hypercore key:", b4a.toString(core.key, 'hex'));
+console.log("core length:", core.length);
 
-process.stdin.on('data', data=> core.append(data));
+const firstBlock = await core.get(3, {valueEncoding:'binary'});
+console.log("first block:", firstBlock)
+// process.stdin.on('data', data=> core.append(data));
 
 swarm.join(core.discoveryKey);
 swarm.on('connection', conn => core.replicate(conn));
